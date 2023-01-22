50f63c3132c1fcf487886bbacb04309f42fb1f32
Ethan <111318733+ethanposh@users.noreply.github.com>
2023-01-22 11:22:15 -0800

    Triggering StateEvents and Observing Results

--- app/src/main/java/com/example/mviexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/mviexample/ui/main/MainViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.mviexample.model.BlogPost
+import com.example.mviexample.model.User
 import com.example.mviexample.ui.main.state.MainStateEvent
 import com.example.mviexample.ui.main.state.MainStateEvent.*
 import com.example.mviexample.ui.main.state.MainViewState
@@ -27,14 +29,79 @@ class MainViewModel: ViewModel() {
     private fun handleStateEvent(stateEvent: MainStateEvent): LiveData<MainViewState> {
         when(stateEvent){// for each case we need return livedata object
             is GetBlogPostEvent -> {
-                return AbsentLiveData.create()
+                //return AbsentLiveData.create()
+
+                //it is for testing dummy livedata
+                return object: LiveData<MainViewState>() {
+                    override fun onActive() {
+                        super.onActive()
+                        val blogList: ArrayList<BlogPost> = ArrayList()
+                        blogList.add(
+                            BlogPost(
+                                pk = 0,
+                                title = "Vancouver PNE 2019",
+                                body = "Here is Jess and I at the Vancouver PNE. We ate a lot of food.",
+                                image = "https://cdn.open-api.xyz/open-api-static/static-blog-images/image8.jpg"
+                            )
+                        )
+                        blogList.add(
+                            BlogPost(
+                                pk = 1,
+                                title = "Ready for a Walk",
+                                body = "Here I am at the park with my dogs Kiba and Maizy. Maizy is the smaller one and Kiba is the larger one.",
+                                image = "https://cdn.open-api.xyz/open-api-static/static-blog-images/image2.jpg"
+                            )
+                        )
+                        value = MainViewState(
+                            blogPosts = blogList
+                        )
+                    }
+                }
             }
             is GetUserEvent -> {
-                return AbsentLiveData.create()
+                //return AbsentLiveData.create()
+
+                //it is for testing by dummy livedata
+                return object: LiveData<MainViewState>() {
+                    override fun onActive() {
+                        super.onActive()
+                        val user = User(
+                            email = "mitch@tabian.ca",
+                            username = "mitch",
+                            image = "https://cdn.open-api.xyz/open-api-static/static-random-images/logo_1080_1080.png"
+                        )
+                        value = MainViewState(
+                            user = user
+                        )
+                    }
+                }
             }
             is None -> {
                 return AbsentLiveData.create()
             }
         }
+    }
+    //2
+    fun setBlogListData(blogPosts: List<BlogPost>){
+        val update = getCurrentViewStateOrNew()
+        update.blogPosts = blogPosts
+        _viewState.value = update
+    }
+    //3
+    fun setUser(user: User){
+        val update = getCurrentViewStateOrNew()
+        update.user = user
+        _viewState.value = update
+    }
+    //1
+    fun getCurrentViewStateOrNew(): MainViewState {
+        val value = viewState.value?.let{
+            it
+        }?: MainViewState()
+        return value
+    }
+    //4
+    fun setStateEvent(event: MainStateEvent){
+        _stateEvent.value = event
     }
 }
